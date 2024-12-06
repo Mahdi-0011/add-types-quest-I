2cc5e5e6a5eae38f89a5f973a39ef476f9af73dd
Mahdi <mahdi.mousavi0011@gmail.com>
2024-12-06 13:07:32 +0100

    d.funktion

--- src/func/dog.ts
+++ src/func/dog.ts
@@ -1,7 +1,8 @@
-import { dog } from "../main";
+import { dog, } from "../main";
 
-export const numberOfColors = (d: dog[]) => {
-
+export const numberOfColors = (dogs: dog[]) => {
+  const uniqueColors = new Set(dogs.map(dog => dog.color));
+  return uniqueColors.size;
 };
 
 export const commonColor = (d: dog[]) => {};
